59f4a9d2edbda9a14efde79ca2bd148f23f38f53
Porutiu Flaviu <flaviu@glitch.ro>
2018-04-22 15:14:32 +0300

    Added migrations

---
+++ database/migrations/2018_04_21_191346_create_participants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('phone');
+            $table->string('email');
+            $table->integer('is_lead')->nullable();
+            $table->integer('team_id');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
